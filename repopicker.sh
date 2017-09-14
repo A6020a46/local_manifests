e85d866ab44a9ddf1c59064fdf219d2d5217b676
Artur <baran0119@users.noreply.github.com>
2017-09-14 17:02:41 +0200

    Update repopicker.sh
    Some cleanup

--- repopicker.sh
+++ repopicker.sh
@@ -1,3 +1,4 @@
+. build/envsetup.sh
 # Already Merged
 #repopick 185798
 # 185521
@@ -7,19 +8,25 @@
 #repopick 186670 186672 186673;
 #repopick 185929 186712;
 #repopick 185801
+#188278 185870 185491
+#186243
+#185636
+#188279 188280
 
 # Abandonned
 #repopick 185639
 # 185852 185869 186505
-
+# 185653 185654 185656 185657 185658 185659 185672
+# 187951
 
 #vendor/lineage + build
-repopick 188278 185870 188401 185491;
+repopick 188401;
+
 #sepolicy
-repopick 186243 186244 186245 186246;
+repopick 186244 186245 186246;
 
 # charger/healthd
-repopick 185642 185653 185654 185656 185657 185658 185659 185672 185888 186304 186881 187146 188765
+repopick 185642 185888 186304 186881 187146 188765
 
 # hardware/qcom/gps
 repopick 186455 186900 186901;
@@ -32,10 +39,10 @@ repopick 186687;
 
 # alarm - bandonned 185639
 # Bionic
-repopick 185636 185640;
+repopick 185640;
 
 #build/soong
-repopick 186740 188279 188280
+repopick 186740
 
 #packages/apps/FMRadio
 repopick 186688
@@ -49,15 +56,11 @@ repopick 188685 188682
 #hardware/qcom/media-caf/msm8916
 repopick 188686
 
-# vendor/lineage healthd - Might conflict
-#repopick 187951;
-
 #system/vold
 repopick 187724 187725 187726 187727 187728 187729 187730 187731 187732 187733 187734 187735
 
 #packages/services/Telephony
 repopick 187147
-
 
 #hardware/libhardware
 repopick 187643 187644
@@ -89,4 +92,3 @@ repopick 186286
 repopick 188687
 #packages/services/Telecomm
 repopick 188873
-
